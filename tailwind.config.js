e1c2104a7cafea8a843178420e4a996b5a1de60b
Akshat Gupta <dev.akshat.py@gmail.com>
2020-05-04 19:33:31 +0530

    vrsn-1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
-    extend: {},
-    fontFamily: {
-      primary: ["Quicksand", "sans-serif"],
+    extend: {
+      fontFamily: {
+        primary: ["Quicksand", "sans-serif"],
+      },
     },
   },
   variants: {},

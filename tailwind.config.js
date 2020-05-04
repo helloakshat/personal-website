3d55804df9ca861f9adef91c48d0c58831bbffc6
Akshat Gupta <dev.akshat.py@gmail.com>
2020-05-04 14:49:30 +0530

    vrsn-1.2 font and styling changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
     extend: {},
+    fontFamily: {
+      primary: ["Quicksand", "sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
